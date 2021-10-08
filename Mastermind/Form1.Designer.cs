8d6cccf1748f87905753814eb24fdf4f974d789a
negawskloV <nathanfaas@ziggo.nl>
2021-10-08 10:03:14 +0200

    Adde How to play button code

--- Mastermind/Form1.Designer.cs
+++ Mastermind/Form1.Designer.cs
@@ -29,13 +29,159 @@ namespace Mastermind
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnWhite = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnPurple = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnHowTo = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.BackColor = System.Drawing.Color.Black;
+            this.btnBlack.Location = new System.Drawing.Point(12, 147);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(40, 40);
+            this.btnBlack.TabIndex = 0;
+            this.btnBlack.UseVisualStyleBackColor = false;
+            // 
+            // btnWhite
+            // 
+            this.btnWhite.BackColor = System.Drawing.Color.White;
+            this.btnWhite.Location = new System.Drawing.Point(12, 193);
+            this.btnWhite.Name = "btnWhite";
+            this.btnWhite.Size = new System.Drawing.Size(40, 40);
+            this.btnWhite.TabIndex = 1;
+            this.btnWhite.UseVisualStyleBackColor = false;
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Green;
+            this.btnGreen.Location = new System.Drawing.Point(12, 331);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(40, 40);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(12, 239);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(40, 40);
+            this.btnRed.TabIndex = 3;
+            this.btnRed.UseVisualStyleBackColor = false;
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(12, 285);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(40, 40);
+            this.btnBlue.TabIndex = 4;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(12, 377);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(40, 40);
+            this.btnYellow.TabIndex = 5;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            // 
+            // btnOrange
+            // 
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.Location = new System.Drawing.Point(12, 423);
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.Size = new System.Drawing.Size(40, 40);
+            this.btnOrange.TabIndex = 6;
+            this.btnOrange.UseVisualStyleBackColor = false;
+            // 
+            // btnPurple
+            // 
+            this.btnPurple.BackColor = System.Drawing.Color.Purple;
+            this.btnPurple.Location = new System.Drawing.Point(12, 469);
+            this.btnPurple.Name = "btnPurple";
+            this.btnPurple.Size = new System.Drawing.Size(40, 40);
+            this.btnPurple.TabIndex = 7;
+            this.btnPurple.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(12, 86);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(90, 55);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit Attempt";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnHowTo
+            // 
+            this.btnHowTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHowTo.Location = new System.Drawing.Point(12, 12);
+            this.btnHowTo.Name = "btnHowTo";
+            this.btnHowTo.Size = new System.Drawing.Size(90, 55);
+            this.btnHowTo.TabIndex = 9;
+            this.btnHowTo.Text = "How to play";
+            this.btnHowTo.UseVisualStyleBackColor = true;
+            this.btnHowTo.Click += new System.EventHandler(this.btnHowTo_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Red;
+            this.btnReset.Location = new System.Drawing.Point(683, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(90, 55);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(785, 651);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnHowTo);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnPurple);
+            this.Controls.Add(this.btnOrange);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnWhite);
+            this.Controls.Add(this.btnBlack);
+            this.Name = "Form1";
+            this.Text = "Mastermind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnWhite;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnPurple;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnHowTo;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
--- Mastermind/Form1.cs
+++ Mastermind/Form1.cs
@@ -16,5 +16,19 @@ namespace Mastermind
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHowTo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The aim of the game is to crack the code. \n " +
+                "You will be given a code by the computer and your goal is to find out what that code is. \n\n " +
+                "First you place what you think the code is, then you click \"Submit Attempt\".\n " +
+                "Then you will see white and red pins appear next to your attempt, a white pin indicates that one of the pins in your attempt is the correct color, but not the correct position. A red pin indicates that a pin in your attempt is both the correct color and position. \n\n " +
+                "Once you guess the code correctly: YOU WIN!!");
+        }
     }
 }
